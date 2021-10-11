8d9c79cf3598e73a2b634bc084ec8d23f4de37fd
Russell Wallace <russell.wallace@gmail.com>
2021-10-11 04:30:46 +0100

    help

--- replac.cc
+++ replac.cc
@@ -5,20 +5,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <iostream>
+using std::cerr;
+using std::cout;
+
+#include <fstream>
+using std::ifstream;
+using std::ofstream;
+
+#include <new>
+using std::set_new_handler;
+
+#include <regex>
+using std::regex;
+using std::regex_match;
+using std::smatch;
+
+#include <string>
+using std::string;
+
 #include <vector>
 using std::vector;
 
 #define version "1"
 
-static void help() {
-	printf("Usage: replac from to files\n"
-		   "\n"
-		   "-h  Show help\n"
-		   "-V  Show version\n");
+void help() {
+	cout << "Usage: replac [options] from to files\n"
+			"\n"
+			"Options:\n"
+			"-h  Show help\n"
+			"-V  Show version\n"
+			"\n"
+			"From: a regular expression\n"
+			"To  : a replacement pattern as in C++ std::regex_replace\n"
+			"\n"
+			"@foo.txt reads args from file foo.txt\n"
+			"This is useful for regular expressions that are hard to specify on the command line\n"
+			"because they use characters the command shell also regards as special;\n"
+			"in that case, from and to can be the first two lines of foo.txt\n"
+			"and files on subsequent lines or in the rest of the command line\n";
 }
 
+void readFile(const string &file, vector<string> &v) {
+	ifstream in(file.c_str());
+	if (!in) {
+		perror(file.c_str());
+		exit(1);
+	}
+	string str;
+	while (getline(in, str)) v.push_back(str);
+}
+
+vector<string> files;
+
 int main(int argc, char **argv) {
-	vector<const char *> files;
+	set_new_handler([]() {
+		perror("new");
+		exit(1);
+	});
+
+	if (argc < 4) {
+		help();
+		return 0;
+	}
+
 	for (int i = 1; i < argc; ++i) {
 		auto s = argv[i];
 		if (*s != '-') {
@@ -33,13 +83,13 @@ int main(int argc, char **argv) {
 			return 0;
 		case 'V':
 		case 'v':
-			printf("replac version " version "\n");
+			cout << "replac version " version "\n";
 			return 0;
 		default:
-			fprintf(stderr, "%s: unknown option\n", argv[i]);
+			cerr << argv[i] << ": unknown option\n";
 			return 1;
 		}
 	}
 
-	for (auto file : files) { puts(file); }
+	for (auto file : files) { cout << (file) << '\n'; }
 }
